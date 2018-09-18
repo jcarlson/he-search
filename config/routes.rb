bd71c0f093aeb50bf460a5eed3016e4cbcebc803
Jarrod Carlson <jarrod.carlson@gmail.com>
2018-09-18 12:23:58 -0600

    Add finishing touches

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'searches#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :searches, path: '/search', as: :search, param: :term, only: [:index, :create, :show]
 end
