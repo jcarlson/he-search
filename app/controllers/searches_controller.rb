06474839b32b7b35808f73d85ef09fcb24b416d7
Jarrod Carlson <jarrod.carlson@gmail.com>
2018-09-18 00:41:12 -0600

    Add simple search form

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,4 +3,15 @@ class SearchesController < ApplicationController
     @search = Search.new
     @trending = Search.trending.limit(10)
   end
+
+  def create
+    @search = Search.create search_params
+    redirect_to search_index_path
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:term)
+  end
 end
